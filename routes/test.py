30f5760b99d4e367929412e10bb8b40e396cdce2
Diiios <68710879+diiios@users.noreply.github.com>
2025-08-11 23:13:53 +0300

    Delete routes directory

--- routes/test.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session, flash, current_app
-
-test = Blueprint('test', __name__)
-
-
-@test.route('/', methods=['GET'])
-def open_test():
-    return render_template('main/test.html')
